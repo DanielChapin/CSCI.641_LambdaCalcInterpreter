4300401e7730b295f6c4a9445ccf5bfdfb38ef9b
Daniel Chapin <dchapes123@gmail.com>
2024-11-13 10:25:17 -0500

    Updated hello_world.human

--- src/ast.hs
+++ src/ast.hs
@@ -1,15 +1,47 @@
 module AST where
 
-data HToken = LCurly | RCurly 
-  | LBracket | RBracket 
-  | LParen | RParen
-  | Star
-  | Comma
-  | IdKw String -- Ids and Keywords
-  | IntLiteral Integer -- ints
-  | StrLiteral String -- strings
-  | BoolLiteral Bool -- booleans
+-- Program ::= module <name>(.<name>)*; Statement*
+--         ::= Statement*
+--
+-- Statement ::= ImportStatement
+--           ::= -- <string>
+--           ::= Def | Out
+--
+-- ImportStatement ::= import <name>;
+--                 ::= import <name> as Import(, Import)*;
+-- Import ::= <name> | ImportFields | <name> ImportFields
+-- ImportFields ::= { ImportField(, ImportField)* }
+-- ImportField ::= <name> | <name> as <name>
+--
+-- Def ::= def <name> = Exp;
+-- Out ::= out <name> = Exp;
+--
+-- Exp ::= \<name>. Exp
+--     ::= (Exp) | Exp Exp
+--     ::= <name> | <name>::<name>
+--     ::= #<name>[<string>] | #<name>![Exp]
+--
+-- <name> ::= [a-zA-Z_][a-zA-Z0-9_]*'*
+-- <string> ::= .*
 
-data HExp = Import String -- import "test.human" as Test::{hello_world, test_func} in
-  | List [HExp] -- lists
-  | If HExp HExp HExp
+data HToken
+  = LBracket
+  | RBracket
+  | LCurly
+  | RCurly
+  | LParen
+  | RParen
+  | Comma
+  | Colon
+  | DoubleColon
+  | Backslash
+  | Hash
+  | IdKw String -- Ids and Keywords (<name>)
+  | Text String
+
+data HExp = Encode String String
+
+data HStatement
+  = Import String (Maybe String) [String] -- import "test.human" as Test::{hello_world, test_func} in
+  | Def String HExp
+  | Evl String String HExp
